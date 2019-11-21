9e4a0202f7abc6d4278a4cdca9be3345dc010612
Alex Vihlayew <vihlayew.alex@gmail.com>
2019-11-21 02:21:18 +0200

    Initial Commit

---
+++ UIOutlineView/UIOutlineView.h
@@ -0,0 +1,19 @@
+//
+//  UIOutlineView.h
+//  UIOutlineView
+//
+//  Created by Alex on 21.11.2019.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIOutlineView.
+FOUNDATION_EXPORT double UIOutlineViewVersionNumber;
+
+//! Project version string for UIOutlineView.
+FOUNDATION_EXPORT const unsigned char UIOutlineViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIOutlineView/PublicHeader.h>
+
+
